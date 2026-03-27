81e9a10bc2d99f34ffbba9b711e9c844e38932d1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-27 00:54:05 -0400

    upstream `.desktop` doesn't work

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,8 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.quickshell.svg
-export DESKTOP=/usr/share/applications/org.quickshell.desktop
+export DESKTOP=DUMMY
+export MAIN_BIN=quickshell
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
